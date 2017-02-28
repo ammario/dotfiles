e237e706bfb5a55ce82a9a9fbdfa77cd3f912d2d
Ammar Bandukwala <ammar@ammar.io>
2017-02-28 12:21:19 -0600

    update install go and make universal

--- installers/install_desktop.sh
+++ installers/install_desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install git vim-gnome i3 i3blocks arandr gdebi synergy keepassx tor torbrowser-launcher wget openvpn
+sudo apt-get install git vim-gnome i3 i3blocks arandr gdebi synergy keepassx tor torbrowser-launcher wget openvpn lsyncd
