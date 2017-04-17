753c6107c11e3f553935e6752b1a53f53874f682
Ammar Bandukwala <ammar@ammar.io>
2017-04-17 04:14:28 -0500

    add font awesome w/ wine fix

--- installers/install_desktop.sh
+++ installers/install_desktop.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-sudo apt-get install git vim-gnome i3 i3blocks arandr gdebi synergy keepassx tor torbrowser-launcher wget openvpn lsyncd qbittorrent
+sudo apt-get install git vim-gnome i3 i3blocks arandr gdebi synergy keepassx tor torbrowser-launcher wget openvpn lsyncd qbittorrent thunar  p7zip-full
 
 #theme stuff
 sudo apt-get install lxappearance arc-theme
+
+#use thunar as default file manager
+xdg-mime default Thunar.desktop inode/directory
 
 
 cd /tmp
