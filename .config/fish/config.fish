af1e3b52cd6926b6b302896c549247b450106145
Ammar Bandukwala <ammar@ammar.io>
2018-07-03 16:26:09 -0500

    Acceptable fish colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,23 @@
 set -g -x fish_greeting ''
 
+# Colors
+
+set pink eb54c6
+set teal 54ebc5
+set blue 54c6eb
+
+set fish_color_cwd $blue
+
 # Git Prompt
-set normal (set_color normal)
-set magenta (set_color magenta)
-set yellow (set_color yellow)
-set green (set_color green)
-set red (set_color red)
-set gray (set_color -o black)
+
 
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'verbose'
-set __fish_git_prompt_color_branch magenta
+set __fish_git_prompt_color $pink
+set __fish_git_prompt_color_branch $blue
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
@@ -25,13 +29,14 @@ set __fish_git_prompt_char_stashstate 's'
 set __fish_git_prompt_char_upstream_ahead '▵'
 set __fish_git_prompt_char_upstream_behind '▿'
 
+# set __fish_git_prompt_show_informative_status "yes"
+
 
 function fish_prompt
   set last_status $status
 
   set_color $fish_color_cwd
   printf '%s' (prompt_pwd)
-  set_color normal
 
   printf '%s ' (__fish_git_prompt)
 
@@ -45,3 +50,7 @@ begin
         source $AUTOJUMP_PATH
     end
 end
+
+# git alias
+alias gpo='git push -u origin HEAD'
+alias gpof='git push --force -u origin HEAD'
