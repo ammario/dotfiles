04231ea313ec0a2b5f084f69ed54ea2244a9e84d
Ammar Bandukwala <ammar@ammar.io>
2018-11-30 11:08:40 -0600

    fish: have Alt+a enter as well

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -110,5 +110,4 @@ set VISUAL nvim
 # See https://fishshell.com/docs/current/commands.html#bind for syntax reference.
 # See https://github.com/fish-shell/fish-shell/blob/23ba7b5bfff9393d222cd8e4a045e3439e24aba1/share/functions/fish_default_key_bindings.fish for default bindings.
 bind -M insert \ew nextd-or-forward-word
-bind -M insert \ea accept-autosuggestion
-
+bind -M insert \ea 'commandline -f accept-autosuggestion execute'
