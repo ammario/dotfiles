417b852becafd20ca9109a8f8d9a36b8a0b023f8
Ammar Bandukwala <ammar@ammar.io>
2018-12-20 15:00:24 -0600

    installers: make install_go version agnostic

--- installers/install_go.sh
+++ installers/install_go.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-VERSION="go1.9"
+if [ $(whoami) != "root" ]; then
+	echo "must be root"
+	exit 1
+fi
+
+VERSION="go1.11.4"
 sudo echo "must sudo now"
 curl https://storage.googleapis.com/golang/$VERSION.linux-amd64.tar.gz | sudo tar -C /usr/local -xzf -
 sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
