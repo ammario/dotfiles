acd3f5d86352dbc4e680dfc4d6a7e961b7f0084c
Ammar Bandukwala <ammar@ammar.io>
2019-07-31 15:28:34 -0500

    Support non-master branches in `git openc`

--- bin/git_open_commit.sh
+++ bin/git_open_commit.sh
@@ -3,4 +3,6 @@ set -ue
 
 commit=$1
 
-python -mwebbrowser "$(hub browse -u)/commit/$commit"
+base_url=$(hub browse -u | grep -Poe "https://github.com/\w*/\w*" )
+
+python -mwebbrowser "${base_url}/commit/$commit"
