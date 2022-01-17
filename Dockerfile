ff28a89ce4ca74d043022a8333cb2517b19bbf7f
Ammar Bandukwala <ammar@ammar.io>
2022-01-17 20:20:37 +0000

    Use script for installer

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM codercom/ubuntu-dev
-
-RUN sudo apt-get -y install fish ripgrep neovim htop
-RUN sudo chsh user -s $(which fish)
-
-ADD .config/fish/ /home/user/.config/fish
-RUN sudo chown -R user:user ~/.config
--- install.sh
+++ install.sh
@@ -1,2 +1,30 @@
-cp -r .bashrc .fonts/ .gitconfig .vim/ .vimrc .inputrc ~
+#!/bin/bash
+cp -r .bashrc .fonts/ .vim/ .vimrc .inputrc ~
+
+if [ ! -f ~/.gitconfig ]; then
+	ln .gitconfig ~/.gitconfig
+fi
+
+set -e
+if $(which apt > /dev/null); then
+	sudo apt install -y neovim fish
+fi
+
+# Replace 'vim' command with neovim.
+if $(which vim > /dev/null); then
+	sudo mv $(which vim) $(mktemp -d)
+	sudo ln -s $(which nvim) /usr/bin/vim	
+fi
+
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Attach fish config
+if [ ! -d ~/.config/fish ]; then
+	ln -s $(pwd)/.config/fish ~/.config
+fi
+
+# Change default shell to fish.
+sudo chsh -s $(which fish) $USER
 
