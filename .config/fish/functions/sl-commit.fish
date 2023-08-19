7a17fc9770bcf9414241ee9d0d70f7cc55a1924c
Ammar Bandukwala <ammar@ammar.io>
2023-08-19 17:38:01 -0500

    Add first version of sl prompt

--- .config/fish/functions/sl-commit.fish
+++ .config/fish/functions/sl-commit.fish
@@ -1,3 +1,3 @@
-function sl-pr-commit
+function sl-commit
     echo (sl log | head -n 1 | awk '{print $2}')
 end
--- .config/fish/functions/sl_prompt.fish
+++ .config/fish/functions/sl_prompt.fish
@@ -1,14 +1,30 @@
 function sl_prompt
-    # Test that .sl directory parents cwd.
-    sl st 2>/dev/null > /dev/null 
+    set LOG_TEMPLATE '{node}|{activebookmark}|{branch}|{github_pull_request_number}|{diffstat}|'
+    set LOG (sl log -l 1 --template $LOG_TEMPLATE 2>/tmp/sl_prompt_error)
     if test $status -ne 0
         return 0
     end
-    set PR_NUM (sl-prnum)
+
     set_color $grey
-    echo " ("
+    echo -n " ("
     set_color $green
-    echo -n "#$PR_NUM"
+
+    set PR_NUM (echo $LOG | awk -v FS='|' '{printf("%s", $4)}' | tr -d '[blank]')
+    set COMMIT (echo $LOG | awk -v FS='|' '{printf("%s", substr($1, 0, 8))}')
+    set BOOKMARK (echo $LOG | awk -v FS='|' '{printf("%s", $2)}')
+
+
+
+    if test -n "$PR_NUM"
+        echo -n '#'
+        echo -n $PR_NUM
+    else if test -n "$BOOKMARK"
+        echo -n $BOOKMARK
+    else
+        echo -n "@"
+        echo -n $COMMIT
+    end
+
     set_color $grey
-    echo ")"
+    echo -n ")"
 end
