1b65d7de51ee4b97597b94f5b9f259a4f41bdd11
Ammar Bandukwala <ammar@ammar.io>
2023-08-19 18:43:26 -0500

    Add remote name to sapling prompt

--- .config/fish/functions/sl_prompt.fish
+++ .config/fish/functions/sl_prompt.fish
@@ -3,7 +3,7 @@ function sl_prompt
     set STATUS_PATH $TMP/status
     sl st > $STATUS_PATH &
 
-    set LOG_TEMPLATE '{node}|{activebookmark}|{branch}|{github_pull_request_number}|{diffstat}|'
+    set LOG_TEMPLATE '{node}|{activebookmark}|{branch}|{github_pull_request_number}|{diffstat}|{remotenames}'
     set LOG (sl log -l 1 --template $LOG_TEMPLATE 2>/tmp/sl_prompt_error)
     if test $status -ne 0
         return 0
@@ -19,11 +19,14 @@ function sl_prompt
         pr_num = $4; gsub(/[ \t]+$/, "", pr_num)
         commit = substr($1, 0, 8)
         bookmark = $2
+        remote = $6
 
         if (pr_num != "") {
             printf "#%s", pr_num
         } else if (bookmark != "") {
             printf "%s", bookmark
+        } else if (remote != "") {
+            printf "%s", remote
         } else {
             printf "@%s", commit
         }
